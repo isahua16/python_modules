aa26503ae96acb7351a71ac6c71464632b488692
isahua16 <isaelhuard@gmail.com>
2023-05-30 20:06:05 -0600

    fixed error message

--- wiki.py
+++ wiki.py
@@ -2,12 +2,13 @@ import wikipedia
 
 while(True):
     try:
-        user_input = input("What would like like to know more about?\n")
+        user_input = input("What page would you like to see?\n")
         sr = wikipedia.page(user_input)
         print(sr.title)
         print(sr.content)
         break
     except wikipedia.exceptions.PageError:
-        print("This page does not exist")
+        print("That page does not exist")
     except:
         print("Something went wrong")
+
